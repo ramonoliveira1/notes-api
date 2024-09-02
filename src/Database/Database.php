0851b4668e7ffae55948dc49b07d1871f55cd666
Ramon Oliveira Novaes <112136464+ramonoliveira1@users.noreply.github.com>
2024-09-02 17:27:33 -0300

    Initial commit

---
+++ src/Database/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $host = 'localhost';
+    private $db = 'passing_notes';
+    private $port = 3306;
+    private $user = 'root';
+    private $pass = '';
+    private $conn;
+
+    public function connect()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;port=$this->port;dbname=$this->db;charset=utf8", $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo 'Erro na conexão com o banco de dados: ' . $e->getMessage();
+            exit;
+        }
+    }
+}
